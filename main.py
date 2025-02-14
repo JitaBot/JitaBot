5196078a6f3a39caedf9d916d015e90e9b58f6f6
JitaBot <furmin2@gmail.com>
2025-02-15 04:25:17 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -1,40 +1,37 @@
+from flask import Flask, request, abort
 import os
-from flask import Flask, request
 from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import random
 
 app = Flask(__name__)
-line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
-handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
 
-def generate_stars():
-    return '⭐' * random.randint(1, 5)
+# 環境変数からLINE Botの情報を取得
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
+LINE_CHANNEL_SECRET = os.getenv('LINE_CHANNEL_SECRET')
 
-@app.route("/callback", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
-    body = request.get_data(as_text=True)
-    handler.handle(body, signature)
-    return 'OK'
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    if event.message.text == "今日の運勢は？":
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=generate_stars())
-        )
-
-# Renderではgunicornで起動するので、ここは不要
-# if __name__ == "__main__":
-#     app.run()
-from flask import Flask
-app = Flask(__name__)
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
 @app.route('/')
 def home():
     return "Hello, world!"
+
+# LINEのWebhookエンドポイント
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+# テキストメッセージを受け取って返信
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text="こんにちは！"))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=10000)
