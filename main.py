1945effa82f3071c1999160c27d486c7da800d84
JitaBot <furmin2@gmail.com>
2025-02-14 20:24:32 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import random
+
+app = Flask(__name__)
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+
+def generate_stars():
+    return '⭐' * random.randint(1, 5)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    handler.handle(body, signature)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    if event.message.text == "今日の運勢":
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=generate_stars()))
+
+if __name__ == "__main__":
+    app.run()
