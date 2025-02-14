03c2603e3d9c2fdbe526c07bbf773157d8821ad6
JitaBot <furmin2@gmail.com>
2025-02-15 04:42:01 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
 from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import os
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
 
-# 環境変数からLINE Botの情報を取得
-LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
-LINE_CHANNEL_SECRET = os.getenv('LINE_CHANNEL_SECRET')
+# LINE APIとハンドラーの設定（環境変数から取得）
+line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
 
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(LINE_CHANNEL_SECRET)
-
-@app.route('/')
-def home():
-    return "Hello, world!"
-
-# LINEのWebhookエンドポイント
+# Webhookエンドポイント
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
@@ -28,34 +21,7 @@ def callback():
         abort(400)
     return 'OK'
 
-# テキストメッセージを受け取って返信
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text="こんにちは！"))
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=10000)
-    from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
-
-app = Flask(__name__)
-
-# LINE APIとハンドラーの設定
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
-
-@app.route("/callback", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
-    body = request.get_data(as_text=True)
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
-
+# メッセージイベント処理
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
@@ -63,5 +29,6 @@ def handle_message(event):
         TextSendMessage(text="Hello, this is JitaBot!")
     )
 
+# アプリ起動
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=10000)
