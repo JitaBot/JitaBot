ab2b41f2af51e514a0582961766fb71e9651eedb
JitaBot <furmin2@gmail.com>
2025-02-16 02:55:39 +0900

    shortener.py を更新

--- shortener.py
+++ shortener.py
@@ -1,18 +1,19 @@
-import requests
 import os
+import requests
+import json
 
 def shorten_url(long_url):
-    api_token = os.environ.get("BITLY_API_TOKEN")
+    token = os.environ.get("BITLY_API_TOKEN")  # Bitly APIトークン環境変数から取得
     url = "https://api-ssl.bitly.com/v4/shorten"
     headers = {
-        "Authorization": f"Bearer {api_token}",
+        "Authorization": f"Bearer {token}",
         "Content-Type": "application/json"
     }
     data = {
         "long_url": long_url,
         "domain": "bit.ly"
     }
-    response = requests.post(url, headers=headers, json=data)
+    response = requests.post(url, headers=headers, data=json.dumps(data))
     if response.status_code == 200:
         return response.json().get("link")
     else:
